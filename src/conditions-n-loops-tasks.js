da363518f05501f622d5cebab774f8f1627ed17f
Reut Vladislav <reut-2011@mail.ru>
2024-01-03 15:39:11 +0300

    feat: add function isContainNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -317,8 +317,16 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let n = num;
+  while (n > 0) {
+    const lastDigit = n % 10;
+    if (lastDigit === digit) {
+      return true;
+    }
+    n = Math.floor(n / 10);
+  }
+  return false;
 }
 
 /**
